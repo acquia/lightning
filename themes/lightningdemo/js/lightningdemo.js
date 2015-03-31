1f68d2d19a248b6cd0e76e3cffd6a00e5efb95e2
Samuel Mortenson <samuel.mortenson@acquia.com>
2015-03-31 13:40:22 -0700

    Fixed min-height of "Moderate" region in lightningdemo theme.

--- themes/lightningdemo/js/lightningdemo.js
+++ themes/lightningdemo/js/lightningdemo.js
@@ -2,10 +2,10 @@
 
   Drupal.behaviors.lightningdemo = {
     attach: function(context, settings) {
-
+      //sometimes the page is shorter than the off canvas moderation, so set a min height based on whatever is in the offcanvas
+      var offcanvasheight = 0; $(".left-off-canvas-menu").children().each(function(){ offcanvasheight += $(this).outerHeight(); });
+      $('.off-canvas-wrap, .left-off-canvas-menu').css('min-height', offcanvasheight + 'px');
     }
-};
+  };
 
 })(jQuery, Drupal);
-
-
