d1dce16aa889cf2c254f59deff6fd8e9d2888a52
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-04 14:16:29 -0500

    Fixed broken drush make command.

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "projects:{lightning:{download:{branch: $TRAVIS_BRANCH, url: https://github.com/$TRAVIS_REPO_SLUG.git}}}" >> env.yml
+echo "projects: {lightning: {download: {branch: $TRAVIS_BRANCH, url: https://github.com/$TRAVIS_REPO_SLUG.git}}}" >> env.yml
