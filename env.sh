39c3c364aa044c510778ebb12710fb40df4592d7
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-04 15:23:46 -0500

    Trying an experiment in env.sh.

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-CI_BRANCH=`git symbolic-ref --short HEAD`
+git branch -a
+CI_BRANCH=`git symbolic-ref --short FETCH_HEAD`
 echo "projects: {lightning: {download: {branch: $CI_BRANCH, url: https://github.com/$TRAVIS_REPO_SLUG.git}}}" >> env.yml
