5d603be42d4461396e1fe67227b97b86aa6ea5ec
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-04 13:42:25 -0500

    Removed invalid -e flag from env.sh.

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo -e "projects:{lightning:{download:{branch: $TRAVIS_BRANCH, url: https://github.com/$TRAVIS_REPO_SLUG.git}}}" >> env.yml
+echo "projects:{lightning:{download:{branch: $TRAVIS_BRANCH, url: https://github.com/$TRAVIS_REPO_SLUG.git}}}" >> env.yml
