e58bd3d678f8d5d160f058a9944e64c89d5ee9ae
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-04 14:30:15 -0500

    Switching env.sh to bash.

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 CI_BRANCH=`git symbolic-ref --short HEAD`
 echo "projects: {lightning: {download: {branch: $CI_BRANCH, url: https://github.com/$TRAVIS_REPO_SLUG.git}}}" >> env.yml
