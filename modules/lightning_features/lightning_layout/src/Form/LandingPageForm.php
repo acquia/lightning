c022cfef775732b039aa008df66781544786a17b
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-14 15:45:24 -0500

    Created the landing page form.

---
+++ modules/lightning_features/lightning_layout/src/Form/LandingPageForm.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\lightning_layout\Form\LandingPageForm.
+ */
+
+namespace Drupal\lightning_layout\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\page_manager\Entity\Page;
+use Drupal\page_manager\Entity\PageVariant;
+
+class LandingPageForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'landing_page';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+      '#required' => TRUE,
+      '#default_value' => $form_state->getValue('title'),
+    ];
+    $form['path'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Path'),
+      '#required' => TRUE,
+      '#default_value' => $form_state->getValue('path'),
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Create'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $path = ltrim(strToLower($form_state->getValue('path')), '/');
+
+    $page = Page::create([
+      'id' => preg_replace('/[^a-z0-9_]+/', '_', $path),
+      'path' => '/' . $path,
+      'label' => $form_state->getValue('title'),
+    ]);
+    $page->save();
+
+    PageVariant::create([
+      'id' => $page->id(),
+      'label' => $page->label(),
+      'page' => $page->id(),
+      'variant' => 'panels_variant',
+      'variant_settings' => [
+        'layout' => 'onecol',
+        // Always use Panels IPE to edit the page's layout and content.
+        'builder' => 'in_place_editor',
+      ],
+    ])->save();
+
+    $form_state->setRedirectUrl(Url::fromUserInput($page->getPath()));
+  }
+
+}
