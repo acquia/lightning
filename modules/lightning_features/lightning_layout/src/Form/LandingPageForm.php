4d45ede6517568067b088803ea83005ddad99c5c
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-15 11:01:19 -0500

    LandingPageForm is now dependency injected for easier testing later.

--- modules/lightning_features/lightning_layout/src/Form/LandingPageForm.php
+++ modules/lightning_features/lightning_layout/src/Form/LandingPageForm.php
@@ -7,13 +7,51 @@
 
 namespace Drupal\lightning_layout\Form;
 
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\Core\Url;
-use Drupal\page_manager\Entity\Page;
-use Drupal\page_manager\Entity\PageVariant;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class LandingPageForm extends FormBase {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $pageStorage;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $pageVariantStorage;
+
+  /**
+   * LandingPageForm constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityStorageInterface $page_storage
+   *   Storage handler for page entities.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $page_variant_storage
+   *   Storage handler for page_variant entities.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface|NULL $translator
+   *   (optional) String translation service.
+   */
+  public function __construct(EntityStorageInterface $page_storage, EntityStorageInterface $page_variant_storage, TranslationInterface $translator = NULL) {
+    $this->pageStorage = $page_storage;
+    $this->pageVariantStorage = $page_variant_storage;
+    $this->stringTranslation = $translator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')->getStorage('page'),
+      $container->get('entity_type.manager')->getStorage('page_variant'),
+      $container->get('string_translation')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -56,14 +94,16 @@ class LandingPageForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $path = ltrim(strToLower($form_state->getValue('path')), '/');
 
-    $page = Page::create([
+    /** @var \Drupal\page_manager\PageInterface $page */
+    $page = $this->pageStorage->create([
       'id' => preg_replace('/[^a-z0-9_]+/', '_', $path),
       'path' => '/' . $path,
       'label' => $form_state->getValue('title'),
     ]);
-    $page->save();
+    $this->pageStorage->save($page);
 
-    PageVariant::create([
+    /** @var \Drupal\page_manager\PageVariantInterface $variant */
+    $variant = $this->pageVariantStorage->create([
       'id' => $page->id(),
       'label' => $page->label(),
       'page' => $page->id(),
@@ -73,7 +113,8 @@ class LandingPageForm extends FormBase {
         // Always use Panels IPE to edit the page's layout and content.
         'builder' => 'in_place_editor',
       ],
-    ])->save();
+    ]);
+    $this->pageVariantStorage->save($variant);
 
     $form_state->setRedirectUrl(Url::fromUserInput($page->getPath()));
   }
