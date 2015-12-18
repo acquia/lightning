233e2d640147137cef639c80ac6a07a6ec7c6fd7
Phéna Proxima <djphenaproxima@gmail.com>
2015-12-18 12:33:49 -0500

    Added methods to our Behat FeatureContext which allow us to get and set text in a WYSIWYG.

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,10 +1,6 @@
 <?php
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
-use Drupal\node\Entity\Node;
-use Drupal\field_collection\Entity\FieldCollectionItem;
-use Drupal\field_collection\Entity\FieldCollection;
-use Drupal\DrupalExtension\Event\EntityEvent;
 use Drupal\Component\Utility\Random;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
@@ -20,14 +16,53 @@ use Behat\Behat\Tester\Exception\PendingException;
 class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   /**
-   * Initializes context.
+   * Fills in WYSIWYG editor with specified ID.
    *
-   * Every scenario gets its own context instance.
-   * You can also pass arbitrary arguments to the
-   * context constructor through behat.yml.
+   * @param string $text
+   *   The text (or HTML) to insert into the WYSIWYG.
+   * @param string $instance_id
+   *   The editor's instance ID in CKEDITOR.instances.
+   *
+   * @When I fill in :text in WYSIWYG editor :instance_id
    */
-  public function __construct() {
+  public function iFillInInWYSIWYGEditor($text, $instance_id)
+  {
+    $this->getSession()
+      ->executeScript("CKEDITOR.instances['$instance_id'].insertHtml('$text')");
+  }
+
+  /**
+   * Asserts that a WYSIWYG editor's data contains a snippet of text.
+   *
+   * @param string $instance_id
+   *   The editor's instance ID in CKEDITOR.instances.
+   * @param string $text
+   *   The text (or HTML) snippet to look for.
+   *
+   * @throws \Exception if the editor doesn't contain the specified text.
+   *
+   * @Then WYSIWYG editor :instance_id should contain :text
+   * @Then the WYSIWYG editor :instance_id should contain :text
+   * @Then the :instance_id WYSIWYG editor should contain :text
+   */
+  public function wysiwygEditorShouldContain($instance_id, $text) {
+    // The JavaScript return value is passed back by Selenium, but Mink's
+    // Selenium2 driver doesn't return it (they're literally missing a return
+    // keyword in executeScript() -- that's all!). So, to get around this,
+    // we need to directly call the appropriate method on the WebDriver wrapper.
+    $html = $this->getSession()
+      ->getDriver()
+      // This method only exists on Mink's Selenium2 driver. But what kind of
+      // loony would try to test a WYSIWYG without Selenium?
+      ->getWebDriverSession()
+      ->execute([
+        'script' => "return CKEDITOR.instances['$instance_id'].getData()",
+        'args' => [],
+      ]);
+
+    if (strpos($html, $text) === FALSE) {
+      throw new \Exception("WYSIWYG editor $instance_id did not contain text $text.");
+    }
   }
 
 }
-
