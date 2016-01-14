3bbd6c23ab2f5bf2e791076e4054b2d7f2863158
Devin Carlson <devin@devincarlson.ca>
2016-01-14 05:47:48 -0500

    Manually excluded modules from diff comparison.

--- scripts/check-overridden.sh
+++ scripts/check-overridden.sh
@@ -3,7 +3,7 @@
 : ${DRUSH:=drush}
 : ${DRUSH_ARGS:=}
 
-LIGHTNING_FEATURES="lightning_admin lightning_article lightning_base lightning_blocks lightning_content lightning_edit lightning_featured lightning_files lightning_filter lightning_forms lightning_fpp lightning_identifiers lightning_image lightning_landing lightning_media lightning_metatags lightning_moderation lightning_page lightning_panels lightning_roles lightning_rules lightning_shortcuts lightning_sps lightning_tags lightning_token lightning_theme lightning_views lightning_wysiwyg"
+LIGHTNING_FEATURES="lightning_admin lightning_article lightning_base lightning_blocks lightning_content lightning_featured lightning_files lightning_filter lightning_forms lightning_fpp lightning_identifiers lightning_image lightning_landing lightning_media lightning_metatags lightning_moderation lightning_page lightning_panels lightning_roles lightning_sps lightning_theme lightning_views lightning_wysiwyg"
 
 # TODO: We should make sure that 'diff' is downloaded first!
 $DRUSH $DRUSH_ARGS en -y diff
