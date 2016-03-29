850ef925d1a3a93e662ad68954e63a3b7fa41ea2
Brant Wynn <brant.wynn@acquia.com>
2016-03-29 11:35:14 -0500

    Updated build.sh to support multiple drush params

--- build-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-MAKEFILE='build-lightning.make'
-TARGET=$1
-CALLPATH=`dirname $0`
-drush make --working-copy --concurrency=5 $CALLPATH/$MAKEFILE $TARGET
--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
 MAKEFILE='build-lightning.make'
-TARGET=$1
 CALLPATH=`dirname $0`
+TARGET=$1
+shift
+
 drush make --concurrency=5 $CALLPATH/$MAKEFILE $TARGET
+
+if [ -d $TARGET ]; then
+  yes "yes" | rm -rf $TARGET
+fi
+
+if [[ ! -z "$@" ]]; then
+  echo "Running drush make with additional params: $@"
+  drush make $CALLPATH/$MAKEFILE $TARGET $@
+else
+  drush make $CALLPATH/$MAKEFILE $TARGET --concurrency=5
+fi
