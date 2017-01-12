9d7d91371085a89fd0d219aad0b4e1db87409122
Adam Hoenich <adam.hoenich@acquia.com>
2017-01-12 10:26:59 -0500

    Replaced block_alter hooks with configurable blacklist.

--- modules/lightning_features/lightning_core/src/Plugin/Block/EntityBlockDeriver.php
+++ modules/lightning_features/lightning_core/src/Plugin/Block/EntityBlockDeriver.php
@@ -2,29 +2,84 @@
 
 namespace Drupal\lightning_core\Plugin\Block;
 
-use Drupal\ctools\Plugin\Deriver\EntityViewDeriver;
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Deriver for the entity_block block plugin.
  */
-class EntityBlockDeriver extends EntityViewDeriver {
+class EntityBlockDeriver extends DeriverBase implements ContainerDeriverInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * EntityBlockDeriver constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $translator
+   *   The string translation service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config_factory, TranslationInterface $translator) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->configFactory = $config_factory;
+    $this->stringTranslation = $translator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('config.factory'),
+      $container->get('string_translation')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function getDerivativeDefinitions($base_plugin_definition) {
-    $derivatives = parent::getDerivativeDefinitions($base_plugin_definition);
+    $blacklist = $this->configFactory
+      ->get('lightning_core.settings')
+      ->get('entity_block.blacklist');
 
-    foreach ($derivatives as $id => $derivative) {
-      $entity_type = $this->entityManager->getDefinition($id);
+    foreach ($this->entityTypeManager->getDefinitions() as $entity_type) {
+      $id = $entity_type->id();
 
-      $derivative['admin_label'] = $entity_type->getLabel();
-      $derivative['description'] = $this->t('Displays a single @entity_type.', [
-        '@entity_type' => $entity_type->getSingularLabel(),
-      ]);
-      unset($derivative['context']);
-
-      $this->derivatives[$id] = $derivative;
+      if (in_array($id, $blacklist)) {
+        continue;
+      }
+      elseif ($entity_type->hasViewBuilderClass()) {
+        $derivative = $base_plugin_definition;
+        $derivative['admin_label'] = $entity_type->getLabel();
+        $derivative['description'] = $this->t('Displays a single @entity_type.', [
+          '@entity_type' => $entity_type->getSingularLabel(),
+        ]);
+        $this->derivatives[$id] = $derivative;
+      }
     }
     return $this->derivatives;
   }
