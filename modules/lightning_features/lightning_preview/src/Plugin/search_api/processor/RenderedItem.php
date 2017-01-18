1a9fbec5f7c9b04936e8c9d5e7fc041b3e19ed11
Adam Hoenich <adam.hoenich@acquia.com>
2017-01-18 12:25:29 -0500

    Made Preview and Search API play nicely via an explicit workspace negotiator.

---
+++ modules/lightning_features/lightning_preview/src/Plugin/search_api/processor/RenderedItem.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\lightning_preview\Plugin\search_api\processor;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\Theme\ThemeInitializationInterface;
+use Drupal\Core\Theme\ThemeManagerInterface;
+use Drupal\lightning_preview\ExplicitWorkspaceNegotiator;
+use Drupal\multiversion\Workspace\WorkspaceManagerInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Plugin\search_api\processor\RenderedItem as BaseRenderedItem;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * A workspace-aware implementation of Search API's rendered_item processor.
+ */
+class RenderedItem extends BaseRenderedItem {
+
+  /**
+   * The workspace manager.
+   *
+   * @var \Drupal\multiversion\Workspace\WorkspaceManagerInterface
+   */
+  protected $workspaceManager;
+
+  /**
+   * The forcible workspace negotiator.
+   *
+   * @var \Drupal\lightning_preview\ForcibleWorkspaceNegotiator
+   */
+  protected $forcibleNegotiator;
+
+  /**
+   * RenderedItem constructor.
+   *
+   * @param array $configuration
+   *   Plugin configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param array $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The currently logged in user.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   * @param \Drupal\Core\Logger\LoggerChannelInterface $logger_channel
+   *   The Search API logger channel.
+   * @param \Drupal\Core\Theme\ThemeManagerInterface $theme_manager
+   *   The theme manager.
+   * @param \Drupal\Core\Theme\ThemeInitializationInterface $theme_initializer
+   *   The theme initializer.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\multiversion\Workspace\WorkspaceManagerInterface $workspace_manager
+   *   The workspace manager.
+   * @param \Drupal\lightning_preview\ExplicitWorkspaceNegotiator $explicit_negotiator
+   *   The explicit workspace negotiator.
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, AccountProxyInterface $current_user, RendererInterface $renderer, LoggerChannelInterface $logger_channel, ThemeManagerInterface $theme_manager, ThemeInitializationInterface $theme_initializer, ConfigFactoryInterface $config_factory, WorkspaceManagerInterface $workspace_manager, ExplicitWorkspaceNegotiator $explicit_negotiator) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->setCurrentUser($current_user);
+    $this->setRenderer($renderer);
+    $this->setLogger($logger_channel);
+    $this->setThemeManager($theme_manager);
+    $this->setThemeInitializer($theme_initializer);
+    $this->setConfigFactory($config_factory);
+    $this->workspaceManager = $workspace_manager;
+    $this->explicitNegotiator = $explicit_negotiator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user'),
+      $container->get('renderer'),
+      $container->get('logger.channel.search_api'),
+      $container->get('theme.manager'),
+      $container->get('theme.initialization'),
+      $container->get('config.factory'),
+      $container->get('workspace.manager'),
+      $container->get('workspace.negotiator.explicit')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+    $workspace_id = $this->workspaceManager->getActiveWorkspace()->id();
+    $this->forcibleNegotiator->setWorkspace($workspace_id);
+    parent::addFieldValues($item);
+  }
+
+}
