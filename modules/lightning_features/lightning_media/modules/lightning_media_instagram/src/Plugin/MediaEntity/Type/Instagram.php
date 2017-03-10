a1efbd78e96c017422e10ba64c01de75e9555aa0
Adam Hoenich <adam.hoenich@acquia.com>
2017-03-10 15:06:23 -0500

    Removed errant interface.

--- modules/lightning_features/lightning_media/modules/lightning_media_instagram/src/Plugin/MediaEntity/Type/Instagram.php
+++ modules/lightning_features/lightning_media/modules/lightning_media_instagram/src/Plugin/MediaEntity/Type/Instagram.php
@@ -3,14 +3,13 @@
 namespace Drupal\lightning_media_instagram\Plugin\MediaEntity\Type;
 
 use Drupal\lightning_media\InputMatchInterface;
-use Drupal\lightning_media\PreviewGeneratorInterface;
 use Drupal\lightning_media\ValidationConstraintMatchTrait;
 use Drupal\media_entity_instagram\Plugin\MediaEntity\Type\Instagram as BaseInstagram;
 
 /**
  * Input-matching version of the Instagram media type.
  */
-class Instagram extends BaseInstagram implements InputMatchInterface, PreviewGeneratorInterface {
+class Instagram extends BaseInstagram implements InputMatchInterface {
 
   use ValidationConstraintMatchTrait;
 
