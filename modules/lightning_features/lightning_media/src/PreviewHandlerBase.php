9d07c7ec229ce53111cba23091b728d825c9aa59
Adam Hoenich <adam.hoenich@acquia.com>
2017-03-13 15:56:22 -0400

    Revert "Refactored media preview code."
    This reverts commit 3bf54b137d234cdf96eab939fc89b9b9f26b7896.

--- modules/lightning_features/lightning_media/src/PreviewHandlerBase.php
+++ modules/lightning_features/lightning_media/src/PreviewHandlerBase.php
@@ -2,28 +2,110 @@
 
 namespace Drupal\lightning_media;
 
+use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\media_entity\MediaInterface;
 
 /**
  * Base class for preview handlers.
- *
- * @deprecated in Lightning 2.0.5 and will be removed in Lightning 2.1.0. Media
- * type plugin definitions should add the 'preview' key instead.
  */
 abstract class PreviewHandlerBase implements PreviewHandlerInterface {
+
+  use DependencySerializationTrait;
+  use SourceFieldTrait;
+  use StringTranslationTrait;
+
+  /**
+   * The media bundle storage handler.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $bundleStorage;
+
+  /**
+   * The storage handler for entity form displays.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $displayStorage;
+
+  /**
+   * PreviewHandlerBase constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $translator
+   *   (optional) The string translation service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_manager, TranslationInterface $translator = NULL) {
+    $this->bundleStorage = $entity_manager->getStorage('media_bundle');
+    $this->fieldStorage = $entity_manager->getStorage('field_config');
+    $this->displayStorage = $entity_manager->getStorage('entity_form_display');
+    $this->stringTranslation = $translator;
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function extraFields() {
-    return lightning_media_entity_extra_field_info();
+  public function extraFields($bundle) {
+    $extra = array();
+
+    if (is_string($bundle)) {
+      $bundle = $this->bundleStorage->load($bundle);
+    }
+
+    if ($bundle instanceof EntityInterface) {
+      $extra['media'][$bundle->id()]['form']['preview'] = [
+        'label' => $this->t('Preview'),
+        'description' => $this->t('A live preview of the @bundle.', [
+          '@bundle' => $bundle->label(),
+        ]),
+        'weight' => 0,
+      ];
+    }
+
+    return $extra;
   }
 
   /**
    * {@inheritdoc}
    */
   public function alterForm(array &$form, FormStateInterface $form_state, EntityInterface $entity = NULL) {
+  }
+
+  /**
+   * Returns the media entity being manipulated by the form.
+   *
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current form state.
+   *
+   * @return \Drupal\media_entity\MediaInterface
+   *   The form's media entity.
+   */
+  protected function getEntity(FormStateInterface $form_state) {
+    /** @var \Drupal\Core\Entity\EntityFormInterface $form */
+    $form = $form_state->getFormObject();
+    return $form->getEntity();
+  }
+
+  /**
+   * Returns the display configuration for the form.
+   *
+   * @param \Drupal\media_entity\MediaInterface $entity
+   *   The media entity whose form is being displayed.
+   * @param string $display_mode
+   *   (optional) The form display mode. Defaults to 'default'.
+   *
+   * @return \Drupal\Core\Entity\Display\EntityFormDisplayInterface
+   *   The form's display configuration.
+   */
+  protected function getDisplay(MediaInterface $entity, $display_mode = NULL) {
+    $id = $entity->getEntityTypeId() . '.' . $entity->bundle() . '.' . ($display_mode ?: 'default');
+    return $this->displayStorage->load($id);
   }
 
 }
--- modules/lightning_features/lightning_media/src/PreviewHandlerInterface.php
+++ modules/lightning_features/lightning_media/src/PreviewHandlerInterface.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\lightning_media;
 
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Defines the interface for a preview handler.
  *
@@ -10,9 +13,31 @@ namespace Drupal\lightning_media;
  * can handle preview differently (depending on the source field type and other
  * factors), so entity forms which will have a preview should use an
  * appropriate implementation of this interface.
- *
- * @deprecated in Lightning 2.0.5 and will be removed in Lightning 2.1.0. Set
- * the 'preview' key on the media type plugin definition instead.
  */
 interface PreviewHandlerInterface {
+
+  /**
+   * Define any extra fields needed to provide a live preview.
+   *
+   * @param \Drupal\media_entity\MediaBundleInterface|string $bundle
+   *   The media bundle, or its ID.
+   *
+   * @return array
+   *   The extra fields, as understood by hook_entity_extra_field_info().
+   */
+  public function extraFields($bundle);
+
+  /**
+   * Alters a media entity form to provide a live preview.
+   *
+   * @param array $form
+   *   The form structure.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current form state.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   (optional) The media entity. If not provided, it will be pulled from
+   *   the form object.
+   */
+  public function alterForm(array &$form, FormStateInterface $form_state, EntityInterface $entity = NULL);
+
 }
