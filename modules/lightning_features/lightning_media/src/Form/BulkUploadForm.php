9ee988a67ff7af6d5495de03be12708b47a1e3bb
Adam Hoenich <adam.hoenich@acquia.com>
2017-03-21 16:01:37 -0400

    A little DI-fu.

--- modules/lightning_features/lightning_media/src/Form/BulkUploadForm.php
+++ modules/lightning_features/lightning_media/src/Form/BulkUploadForm.php
@@ -2,13 +2,46 @@
 
 namespace Drupal\lightning_media\Form;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * A form for uploading multiple media assets at once.
  */
 class BulkUploadForm extends FormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * BulkUploadForm constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $translator
+   *   The string translation service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, TranslationInterface $translator) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->setStringTranslation($translator);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('string_translation')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -39,8 +72,13 @@ class BulkUploadForm extends FormBase {
     $uploads = $form_state->getValue(['dropzone', 'uploaded_files']);
 
     foreach ($uploads as $upload) {
-      // $upload['path'] is a URI
-      // $upload['filename'] is...the filename, durr
+      // Create a file entity around the temporary file.
+      $file = $this->entityTypeManager->getStorage('file')->create([
+        'uri' => $upload['path'],
+      ]);
+      // Determine the media bundle that applies.
+      // Create a media entity and save it.
+      // Add the entity to the bulk creation queue.
     }
   }
 
