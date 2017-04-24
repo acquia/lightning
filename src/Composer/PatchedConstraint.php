dd63e47d24abc8a58f139d30847e56d3c0ae7070
Adam Balsam <balsam.adam@gmail.com>
2017-04-24 14:02:43 -0400

    WIP patched constraints

--- src/Composer/PatchedConstraint.php
+++ src/Composer/PatchedConstraint.php
@@ -3,6 +3,7 @@
 namespace Acquia\Lightning\Composer;
 
 use Composer\Json\JsonFile;
+use Composer\Script\Event;
 
 /**
  * Ensures that all patched dependencies are pinned to a specific version.
@@ -11,12 +12,11 @@ class PatchedConstraint {
   private $json;
   private $patched_dependencies;
 
-  public function __construct() {
-    $app_root = \Drupal::root();
-    $json_file = new JsonFile($app_root . '/../composer.json');
-    $this->json = $json_file->read();
-    $this->patched_dependencies = $this->getPatchedDependencyConstraints();
+  public static function execute(Event $event) {
+    $root_package = $event->getComposer()->getPackage();
+    $patched_dependencies = self::getPatchedDependencyConstraints();
   }
+
 
   /**
    * Gets a list of required packages that also have patches defined.
@@ -28,7 +28,9 @@ class PatchedConstraint {
    *   List of required packages which are patched and their version constraint.
    */
   protected function getPatchedDependencyConstraints($ignore = ['drupal/core']) {
-    $json = $this->json;
+    $app_root = \Drupal::root();
+    $json_file = new JsonFile($app_root . '/../composer.json');
+    $json = $json_file->read();
     $patched = array_keys($json['extra']['patches']);
     $patched_dependencies = array_intersect_key($json['require'], array_flip($patched));
     // @todo strip out ignored dependencies.
