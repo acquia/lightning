727f2128e4861686c75419a9f6d31fafe439cc28
Adam Balsam <adam.balsam@acquia.com>
2017-06-06 18:49:18 -0400

    just confirming that I _cant_ do this

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -9,6 +9,7 @@ target_env="$2"
 
 # Create the config directories defined in the settings file by pipelines.
 mkdir -p /var/www/html/$site.$target_env/docroot/sites/default/files/config/sync
+mkdir -p /var/www/html/$site.$target_env/config/default
 
 # Fresh install of Lightning.
 drush @$site.$target_env site-install lightning --account-pass=admin --yes
