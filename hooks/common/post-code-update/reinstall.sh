b2e0550d797701ae3981849dca685a53127b6244
Adam Balsam <adam.balsam@acquia.com>
2017-06-06 18:02:24 -0400

    try perms

--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -7,5 +7,8 @@
 site="$1"
 target_env="$2"
 
+# Create the config sync directory defined in the settings file by pipelines.
 mkdir -p /var/www/html/$site.$target_env/docroot/sites/default/files/config/sync
+
+# Fresh install of Lightning.
 drush @$site.$target_env site-install lightning --account-pass=admin --yes
