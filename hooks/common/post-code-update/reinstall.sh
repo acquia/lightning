42af8b9f2439c679deacf46f57ca82b2ff474c40
Adam Balsam <adam.balsam@acquia.com>
2017-06-06 20:44:37 -0400

    make the folder cloud expects from pipelines

--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -7,9 +7,5 @@
 site="$1"
 target_env="$2"
 
-# Create the config directories defined in the settings file by pipelines.
-chmod -R +w /var/www/html/$site.$target_env/config/
-mkdir -p /var/www/html/$site.$target_env/config/default
-
 # Fresh install of Lightning.
 drush @$site.$target_env site-install lightning --account-pass=admin --yes
