e44b3cdb103157b44a82ee7e0b70215ece127597
Adam Hoenich <adam.hoenich@acquia.com>
2017-06-28 11:34:43 -0400

    Fixed a bug where embed configuration was being thrown out.

--- modules/lightning_features/lightning_media/src/Form/EntityEmbedDialog.php
+++ modules/lightning_features/lightning_media/src/Form/EntityEmbedDialog.php
@@ -13,16 +13,19 @@ class EntityEmbedDialog extends BaseEntityEmbedDialog {
    */
   public function buildEmbedStep(array $form, FormStateInterface $form_state) {
     $entity = $form_state->get('entity');
-    $entity_element = $form_state->get('entity_element');
+    $element = $form_state->get('entity_element');
+    $input = $form_state->getUserInput();
 
-    // If the entity being embedded is a media item, see if its handler prefers
-    // a certain Entity Embed display plugin.
-    if ($entity instanceof MediaInterface) {
+    // If we're working with an existing embed, $input['editor_object'] will be
+    // set, in which case we don't want to change anything (see ::buildForm()).
+    // Otherwise, if the entity being embedded is a media item, see if its type
+    // plugin has a preference regarding which display plugin to use.
+    if (empty($input['editor_object']) && $entity instanceof MediaInterface) {
       $plugin_definition = $entity->getType()->getPluginDefinition();
 
       if (isset($plugin_definition['entity_embed_display'])) {
-        $entity_element['data-entity-embed-display'] = $plugin_definition['entity_embed_display'];
-        $form_state->set('entity_element', $entity_element);
+        $element['data-entity-embed-display'] = $plugin_definition['entity_embed_display'];
+        $form_state->set('entity_element', $element);
       }
     }
     return parent::buildEmbedStep($form, $form_state);
