28a754818bb429a4d5f3828e1765abf8e301b9bc
Adam Hoenich <adam.hoenich@acquia.com>
2017-09-14 09:58:23 -0400

    Added inline command documentation.

--- RoboFile.php
+++ RoboFile.php
@@ -14,6 +14,32 @@ class RoboFile extends \Robo\Tasks {
       ->stopOnFail();
   }
 
+  /**
+   * Run Behat tests.
+   *
+   * To run all tests, simply run 'test:behat'. To run a specific a specific
+   * feature, you can pass its path, relative to the tests/features directory:
+   *
+   * test:behat -- media/image.feature
+   *
+   * You can omit the .feature extension. This example runs
+   * tests/features/workflow/diff.feature:
+   *
+   * test:behat -- workflow/diff
+   *
+   * This also works with a directory of features. This example runs everything
+   * in tests/features/media:
+   *
+   * test:behat -- media
+   *
+   * Any command-line options after the initial -- will be passed unmodified to
+   * Behat. So you can filter tests by tags, like normal:
+   *
+   * test:behat -- --tags=javascript,~media
+   *
+   * This command will start Selenium Server in the background during the test
+   * run, to support functional JavaScript tests.
+   */
   public function testBehat(array $arguments) {
     $this
       ->taskExec('bin/selenium-server-standalone')
