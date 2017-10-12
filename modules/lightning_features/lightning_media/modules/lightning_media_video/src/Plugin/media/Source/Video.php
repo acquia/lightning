abcf1771d7816d85cdae07b204b6754fc9fce196
Adam Hoenich <adam.hoenich@acquia.com>
2017-10-12 12:17:51 -0400

    Broken port of video module.

---
+++ modules/lightning_features/lightning_media/modules/lightning_media_video/src/Plugin/media/Source/Video.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\lightning_media_video\Plugin\media\Source;
+
+use Drupal\lightning_media\InputMatchInterface;
+use Drupal\media\MediaTypeInterface;
+use Drupal\video_embed_media\Plugin\media\Source\VideoEmbedField;
+
+/**
+ * Input-matching version of the VideoEmbedField media type.
+ */
+class Video extends VideoEmbedField implements InputMatchInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function appliesTo($value, MediaTypeInterface $media_type) {
+    return (boolean) $this->providerManager->loadProviderFromInput($value);
+  }
+
+}
