6049eed20ae50eded278e9e72b57f9ed0b8dedd5
Adam Hoenich <adam.hoenich@acquia.com>
2017-10-18 12:02:50 -0400

    Working to eliminate PHP warnings and errors.

--- modules/lightning_features/lightning_media/modules/lightning_media_video/src/Plugin/media/Source/Video.php
+++ modules/lightning_features/lightning_media/modules/lightning_media_video/src/Plugin/media/Source/Video.php
@@ -15,9 +15,30 @@ class Video extends VideoEmbedField implements InputMatchInterface {
    * {@inheritdoc}
    */
   public function appliesTo($value, MediaTypeInterface $media_type) {
-    return is_string($value)
+    $value = $this->toString($value);
+
+    return isset($value)
       ? (bool) $this->providerManager->loadProviderFromInput($value)
       : FALSE;
+  }
+
+  /**
+   * Safely converts a value to a string.
+   *
+   * The value is converted if it is either scalar, or an object with a
+   * __toString() method.
+   *
+   * @param mixed $value
+   *   The value to convert.
+   *
+   * @return string|null
+   *   The string representation of the value, or NULL if the value cannot be
+   *   converted to a string.
+   */
+  protected function toString($value) {
+    return is_scalar($value) || (is_object($value) && method_exists($value, '__toString'))
+      ? (string) $value
+      : NULL;
   }
 
 }
