c22ec1b61adbef54cdf6e9e553c5f1467df3471b
Adam Hoenich <adam.hoenich@acquia.com>
2017-10-27 10:13:03 -0400

    Ripped out the forward revision indicator on the content view, since Content Moderation does not support it yet.

--- modules/lightning_features/lightning_workflow/src/Plugin/views/field/ForwardRevisionExists.php
+++ modules/lightning_features/lightning_workflow/src/Plugin/views/field/ForwardRevisionExists.php
@@ -2,43 +2,10 @@
 
 namespace Drupal\lightning_workflow\Plugin\views\field;
 
-use Drupal\Core\Render\Markup;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
-use Drupal\views\ResultRow;
 
 /**
  * A Views field to indicate if a content entity has forward revision(s).
- *
- * @ViewsField("forward_revision_exists")
  */
 class ForwardRevisionExists extends FieldPluginBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function render(ResultRow $values) {
-    // This depends on the view having a relationship to the latest revision.
-    // It's not clear if it's even possible for this handler to enforce that,
-    // though, so we'll make do with a soft check.
-    $rel = 'latest_revision__' . $this->view->getBaseEntityType()->id();
-
-    if (empty($this->view->relationship[$rel])) {
-      return NULL;
-    }
-
-    /** @var \Drupal\Core\Entity\ContentEntityInterface $current */
-    $current = $values->_entity;
-
-    if (!isset($values->_relationship_entities[$rel])) {
-      return NULL;
-    }
-    /** @var \Drupal\Core\Entity\ContentEntityInterface $latest */
-    elseif (($latest = $values->_relationship_entities[$rel]) && ($latest->getRevisionId() > $current->getRevisionId())) {
-      return Markup::create('&#10003;');
-    }
-    else {
-      return NULL;
-    }
-  }
-
 }
