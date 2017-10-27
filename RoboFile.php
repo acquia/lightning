d19e87b16b3d2ba6c9b395eefdde6badaa037608
Adam Balsam <adam.balsam@acquia.com>
2017-10-27 16:01:08 -0400

    Remove manual cache rebuild and instead patch Media Entity so that it doesn't cache media type plugins

--- RoboFile.php
+++ RoboFile.php
@@ -40,9 +40,6 @@ class RoboFile extends \Robo\Tasks {
     if ($tasks) {
       $tasks
         ->addTask(
-          $this->taskDrush('cr')
-        )
-        ->addTask(
           $this->taskDrush('updatedb')->option('yes')
         )
         ->addTask(
