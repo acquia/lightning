3ba21ee70d6e7249169ef74503978a927f43af5f
Adam Hoenich <adam.hoenich@acquia.com>
2017-12-06 13:35:32 -0500

    Adjusted UpdateCommandTest.

--- tests/src/Unit/UpdateCommandTest.php
+++ tests/src/Unit/UpdateCommandTest.php
@@ -3,13 +3,9 @@
 namespace Drupal\Tests\lightning\Unit;
 
 use Drupal\Component\Plugin\Discovery\DiscoveryInterface;
-use Drupal\Core\Config\Config;
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\DependencyInjection\ClassResolver;
 use Drupal\lightning\Command\UpdateCommand;
 use Drupal\Tests\UnitTestCase;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * @coversDefaultClass \Drupal\lightning\Command\UpdateCommand
@@ -17,13 +13,6 @@ use Symfony\Component\Console\Output\OutputInterface;
  * @group lightning
  */
 class UpdateCommandTest extends UnitTestCase {
-
-  /**
-   * The mocked config containing the known versions.
-   *
-   * @var Config
-   */
-  protected $versions;
 
   /**
    * The command object under test.
@@ -37,11 +26,6 @@ class UpdateCommandTest extends UnitTestCase {
    */
   protected function setUp() {
     parent::setUp();
-
-    $this->versions = $this->prophesize(Config::class);
-
-    $config_factory = $this->prophesize(ConfigFactoryInterface::class);
-    $config_factory->get('lightning.versions')->willReturn($this->versions->reveal());
 
     $discovery = $this->prophesize(DiscoveryInterface::class);
     $discovery->getDefinitions()->willReturn([
@@ -62,73 +46,16 @@ class UpdateCommandTest extends UnitTestCase {
     $this->command = new TestUpdateCommand(
       new ClassResolver(),
       new \ArrayIterator(),
-      $config_factory->reveal(),
       $discovery->reveal()
     );
   }
 
-  public function testProviderVersionIsKnown() {
-    $this->versions->get()->willReturn([
-      'fubar' => '1.2.2',
-    ]);
+  public function testSince() {
+    $this->command->since = '1.2.2';
 
     $definitions = $this->command->getDefinitions();
     $this->assertCount(1, $definitions);
     $this->assertArrayHasKey('fubar:1.2.3', $definitions);
-  }
-
-  public function testProviderVersionNotKnown() {
-    $this->versions->get()->willReturn([]);
-
-    $this->command->profileInfo = ['version' => '8.x-1.22'];
-
-    $definitions = $this->command->getDefinitions();
-    $this->assertCount(1, $definitions);
-    $this->assertArrayHasKey('fubar:1.2.3', $definitions);
-  }
-
-  /**
-   * @depends testProviderVersionIsKnown
-   */
-  public function testSinceWithKnownProviderVersion() {
-    $this->versions->get()->willReturn([
-      'fubar' => '1.2.2',
-    ]);
-    $this->command->since = '1.2.0';
-
-    $definitions = $this->command->getDefinitions();
-    $this->assertCount(3, $definitions);
-    $this->assertArrayHasKey('fubar:1.2.1', $definitions);
-    $this->assertArrayHasKey('fubar:1.2.2', $definitions);
-    $this->assertArrayHasKey('fubar:1.2.3', $definitions);
-  }
-
-  /**
-   * @depends testProviderVersionNotKnown
-   */
-  public function testSinceWithUnknownProviderVersion() {
-    $this->versions->get()->willReturn([]);
-
-    $this->command->since = '1.2.0';
-    $this->command->profileInfo = ['version' => '8.x-1.22'];
-
-    $definitions = $this->command->getDefinitions();
-    $this->assertCount(3, $definitions);
-    $this->assertArrayHasKey('fubar:1.2.1', $definitions);
-    $this->assertArrayHasKey('fubar:1.2.2', $definitions);
-    $this->assertArrayHasKey('fubar:1.2.3', $definitions);
-  }
-
-  /**
-   * @depends testSinceWithKnownProviderVersion
-   * @depends testSinceWithUnknownProviderVersion
-   */
-  public function testForce() {
-    $input = $this->prophesize(InputInterface::class);
-    $input->getOption('force')->willReturn(TRUE);
-
-    $this->command->initialize($input->reveal(), $this->prophesize(OutputInterface::class)->reveal());
-    $this->assertSame('0.0.0', $this->command->since);
   }
 
 }
@@ -139,18 +66,6 @@ final class TestUpdateCommand extends UpdateCommand {
    * {@inheritdoc}
    */
   public $since;
-
-  /**
-   * {@inheritdoc}
-   */
-  public $profileInfo;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function initialize(InputInterface $input, OutputInterface $output) {
-    parent::initialize($input, $output);
-  }
 
   /**
    * {@inheritdoc}
