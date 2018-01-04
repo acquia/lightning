7e1368d9212d2641dc33416ab5aaace1ec562e99
Phéna Proxima <djphenaproxima@gmail.com>
2018-01-04 10:42:03 -0500

    Fixed ComponentDiscoveryTest.

--- tests/src/Kernel/ComponentDiscoveryTest.php
+++ tests/src/Kernel/ComponentDiscoveryTest.php
@@ -50,9 +50,16 @@ class ComponentDiscoveryTest extends KernelTestBase {
   public function testGetMainComponents() {
     $components = $this->discovery->getMainComponents();
 
+    $this->assertInstanceOf(Extension::class, $components['lightning_api']);
     $this->assertInstanceOf(Extension::class, $components['lightning_core']);
+    $this->assertInstanceOf(Extension::class, $components['lightning_layout']);
+    $this->assertInstanceOf(Extension::class, $components['lightning_media']);
+    $this->assertInstanceOf(Extension::class, $components['lightning_workflow']);
+
+    $this->assertArrayNotHasKey('lightning_contact_form', $components);
+    $this->assertArrayNotHasKey('lightning_page', $components);
+    $this->assertArrayNotHasKey('lightning_roles', $components);
     $this->assertArrayNotHasKey('lightning_search', $components);
-    $this->assertArrayNotHasKey('lightning_dev', $components);
   }
 
   /**
@@ -61,8 +68,10 @@ class ComponentDiscoveryTest extends KernelTestBase {
   public function testGetSubComponents() {
     $components = $this->discovery->getSubComponents();
 
+    $this->assertInstanceOf(Extension::class, $components['lightning_contact_form']);
+    $this->assertInstanceOf(Extension::class, $components['lightning_page']);
+    $this->assertInstanceOf(Extension::class, $components['lightning_roles']);
     $this->assertInstanceOf(Extension::class, $components['lightning_search']);
-    $this->assertInstanceOf(Extension::class, $components['lightning_dev']);
     $this->assertArrayNotHasKey('lightning_core', $components);
   }
 
