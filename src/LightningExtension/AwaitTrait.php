7e271211c62f5ce3fd836f9c8aef113b636b9da5
Adam Hoenich <adam.hoenich@acquia.com>
2018-03-26 09:56:15 -0400

    Switched to using __NAMESPACE__ magic in deprecation notice.

--- src/LightningExtension/AwaitTrait.php
+++ src/LightningExtension/AwaitTrait.php
@@ -2,7 +2,7 @@
 
 namespace Acquia\LightningExtension;
 
-@trigger_error('\Acquia\LightningExtension\AwaitTrait is deprecated. Use \Acquia\LightningExtension\Context\AwaitTrait instead.', E_USER_DEPRECATED);
+@trigger_error(__NAMESPACE__ . '\AwaitTrait is deprecated. Use ' . __NAMESPACE__ . '\Context\AwaitTrait instead.', E_USER_DEPRECATED);
 
 use Acquia\LightningExtension\Context\AwaitTrait as BaseAwaitTrait;
 
