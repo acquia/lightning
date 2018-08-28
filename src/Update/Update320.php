6b6f48ee3fbc2467d78675d020e516ca755f2c55
Adam <adam@phenaproxima.net>
2018-08-28 11:20:00 -0400

    Update320 converts sub-profiles incorrectly (#598)

--- src/Update/Update320.php
+++ src/Update/Update320.php
@@ -4,6 +4,7 @@ namespace Drupal\lightning\Update;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Extension\ExtensionDiscovery;
+use Drupal\Core\Extension\ProfileExtensionList;
 use Drupal\Core\Serialization\Yaml;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\StringTranslation\TranslationInterface;
@@ -25,15 +26,25 @@ final class Update320 implements ContainerInjectionInterface {
   protected $appRoot;
 
   /**
+   * The profile extension list service.
+   *
+   * @var \Drupal\Core\Extension\ProfileExtensionList
+   */
+  protected $profileList;
+
+  /**
    * Update320 constructor.
    *
    * @param string $app_root
    *   The Drupal application root.
+   * @param \Drupal\Core\Extension\ProfileExtensionList $profile_list
+   *   The profile extension list service.
    * @param \Drupal\Core\StringTranslation\TranslationInterface|NULL $translation
    *   (optional) The string translation service.
    */
-  public function __construct($app_root, TranslationInterface $translation = NULL) {
+  public function __construct($app_root, ProfileExtensionList $profile_list, TranslationInterface $translation = NULL) {
     $this->appRoot = $app_root;
+    $this->profileList = $profile_list;
 
     if ($translation) {
       $this->setStringTranslation($translation);
@@ -46,6 +57,7 @@ final class Update320 implements ContainerInjectionInterface {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('app.root'),
+      $container->get('extension.list.profile'),
       $container->get('string_translation')
     );
   }
@@ -55,7 +67,7 @@ final class Update320 implements ContainerInjectionInterface {
    *
    * @update
    *
-   * @ask Do you want to update all sub-profiles to be Drupal 8.6 compatible?
+   * @ask Do you want to update all sub-profiles to be Drupal 8.6-compatible?
    *
    * @param \Symfony\Component\Console\Style\StyleInterface $io
    *   The I/O handler.
@@ -63,51 +75,56 @@ final class Update320 implements ContainerInjectionInterface {
   public function updateProfiles(StyleInterface $io) {
     $discovery = new ExtensionDiscovery($this->appRoot);
 
-    $profiles = $discovery->scan('profile');
-    foreach ($profiles as $profile) {
+    foreach ($discovery->scan('profile') as $profile) {
       $info_file = $profile->getPathname();
 
-      if (is_writeable($info_file)) {
-        $info = file_get_contents($info_file);
-
-        if (strstr($info, 'base profile:')) {
-          $info = Yaml::decode($info);
-
-          if (is_array($info['base profile'])) {
-            $info['base profile'] = $info['base profile']['name'];
-          }
-          if (isset($info['dependencies'])) {
-            $info['install'] = $info['dependencies'];
-            unset($info['dependencies']);
-          }
-
-          $exclude = [];
-          if (isset($info['excluded_dependencies'])) {
-            $exclude = array_merge($exclude, $info['excluded_dependencies']);
-            unset($info['excluded_dependencies']);
-          }
-          if (isset($info['excluded_themes'])) {
-            $exclude = array_merge($exclude, $info['excluded_themes']);
-            unset($info['excluded_themes']);
-          }
-          if ($exclude) {
-            $info['exclude'] = $exclude;
-          }
-          file_put_contents($info_file, Yaml::encode($info));
-
-          $message = $this->t('Updated @profile.', [
-            '@profile' => $profile->getName(),
-          ]);
-          $io->success((string) $message);
-        }
-      }
-      else {
+      if (! is_writeable($info_file)) {
         $message = $this->t('Cannot write to @path, skipping.', [
           '@path' => $info_file,
         ]);
         $io->warning((string) $message);
+        continue;
+      }
+
+      $info = file_get_contents($info_file);
+      if (empty($info) || ! strstr($info, 'base profile:')) {
+        continue;
+      }
+
+      $info = Yaml::decode($info);
+
+      // 'base profile' must be an array with at least the 'name' key.
+      if (! isset($info['base profile']['name'])) {
+        continue;
+      }
+
+      // Build the list of excluded extensions and themes.
+      $exclude = [];
+      if (isset($info['base profile']['excluded_dependencies'])) {
+        $exclude = array_merge($exclude, $info['base profile']['excluded_dependencies']);
+      }
+      if (isset($info['base profile']['excluded_themes'])) {
+        $exclude = array_merge($exclude, $info['base profile']['excluded_themes']);
+      }
+
+      $info['base profile'] = $info['base profile']['name'];
+      if ($exclude) {
+        $info['exclude'] = $exclude;
+      }
+
+      $success = file_put_contents($info_file, Yaml::encode($info));
+      if ($success) {
+        $message = $this->t('Updated @profile.', [
+          '@profile' => $profile->getName(),
+        ]);
+        $io->success((string) $message);
+      }
+      else {
+        throw new \RuntimeException('An error occurred writing to ' . $profile->getPathname());
       }
     }
+    // Reset the cached profile list.
+    $this->profileList->reset();
   }
 
 }
