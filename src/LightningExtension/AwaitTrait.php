83a50c923d0622a719019dc15eed756341c0adb7
Phéna Proxima <adam@phenaproxima.net>
2019-07-18 12:29:57 -0400

    Issue #3068751 by phenaproxima: Deprecate unused Behat contexts

--- src/LightningExtension/AwaitTrait.php
+++ src/LightningExtension/AwaitTrait.php
@@ -2,7 +2,7 @@
 
 namespace Acquia\LightningExtension;
 
-@trigger_error(__NAMESPACE__ . '\AwaitTrait is deprecated in lightning:8.x-4.0 and will be removed in lightning:8.x-4.1. Use ' . __NAMESPACE__ . '\Context\AwaitTrait instead. See https://www.drupal.org/node/2105097', E_USER_DEPRECATED);
+@trigger_error(__NAMESPACE__ . '\AwaitTrait is deprecated in lightning:8.x-4.0 and will be removed in lightning:8.x-4.1. Use ' . __NAMESPACE__ . '\Context\AwaitTrait instead. See https://www.drupal.org/node/3068751', E_USER_DEPRECATED);
 
 use Acquia\LightningExtension\Context\AwaitTrait as BaseAwaitTrait;
 
--- src/LightningExtension/DetailsTrait.php
+++ src/LightningExtension/DetailsTrait.php
@@ -2,6 +2,8 @@
 
 namespace Acquia\LightningExtension;
 
+@trigger_error(__NAMESPACE__ . '\DetailsTrait is deprecated in lightning:8.x-4.0 and will be removed in lightning:8.x-4.1. If you need its functionality, you should copy the relevant code into your own project. See https://www.drupal.org/node/3068751', E_USER_DEPRECATED);
+
 use Behat\Mink\Element\ElementInterface;
 use Behat\Mink\Exception\ExpectationException;
 
@@ -25,6 +27,8 @@ trait DetailsTrait {
    *   If the element is not found in the container.
    */
   protected function assertDetails($summary, ElementInterface $container) {
+    @trigger_error(__METHOD__ . ' is deprecated in lightning:8.x-4.0 and will be removed in lightning:8.x-4.1. If you need its functionality, you should copy the relevant code into your own project. See https://www.drupal.org/node/3068751', E_USER_DEPRECATED);
+
     /** @var \Behat\Mink\Element\NodeElement $element */
     foreach ($container->findAll('css', 'details > summary') as $element) {
       if ($element->getText() == $summary) {
