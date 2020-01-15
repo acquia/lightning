175d791bc72697b762df592bd5a236395c0e3ff7
Phéna Proxima <adam@phenaproxima.net>
2020-01-15 11:48:42 -0500

    [ORCA] Ensure that components are treated as part of the SUT when installing a SUT-only fixture.

--- tests/travis/install.sh
+++ tests/travis/install.sh
@@ -14,21 +14,31 @@ cd "$(dirname "$0")"
 # Reuse ORCA's own includes.
 source ../../../orca/bin/travis/_includes.sh
 
+# Handle the special case of scanning for deprecations in contrib dependencies.
+# We need to ensure that the components are included as part of the SUT,
+# but none of the other Acquia product modules are.
 if [[ "$ORCA_JOB" == "DEPRECATED_CODE_SCAN_CONTRIB" ]]; then
-  orca fixture:init -f --sut="$ORCA_SUT_NAME" --sut-only --no-site-install
+  export ORCA_PACKAGES_CONFIG=../lightning/tests/packages.yml
+  orca fixture:init -f --sut="$ORCA_SUT_NAME" --dev --no-site-install
   exit 0
 fi
 
-../../../orca/bin/travis/install.sh
-
-# This is a temporary workaround for a change in BLT 11.x which causes
-# mikey179/vfsstream to be absent from the fixture, which breaks all
-# kernel tests.
-if [ -d $ORCA_FIXTURE_DIR ]; then
-  cd $ORCA_FIXTURE_DIR
-  composer require --dev mikey179/vfsstream weitzman/drupal-test-traits
+# When testing the SUT in isolation using dev package versions, treat the
+# components as part of the SUT, to be installed in an isolated (SUT-only)
+# fixture.
+if [[ "$ORCA_JOB" == "ISOLATED_DEV" ]]; then
+  export ORCA_PACKAGES_CONFIG=../lightning/tests/packages.yml
+  orca fixture:init -f --sut="$ORCA_SUT_NAME" --dev --profile=lightning
+else
+  # Run ORCA's standard installation script.
+  ../../../orca/bin/travis/install.sh
 fi
-# End temporary workaround.
+
+# If there is no fixture, there's nothing else for us to do.
+[[ -d "$ORCA_FIXTURE_DIR" ]] || exit 0
+
+# Add testing dependencies.
+composer -d"$ORCA_FIXTURE_DIR" require --dev weitzman/drupal-test-traits
 
 # Exit early if no DB fixture is specified.
 [[ "$DB_FIXTURE" ]] || exit 0
