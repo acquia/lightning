cbc4a8aab758d2f38b26c7914583a90f9cab6569
kdruckman <kdruckman@gmail.com>
2020-04-02 12:34:43 -0500

    more wip

--- tests/src/Functional/SubprofileGeneratorTest.php
+++ tests/src/Functional/SubprofileGeneratorTest.php
@@ -1,23 +1,23 @@
 <?php
 
-
 namespace Drupal\Tests\lightning\Functional;
 
 use Drupal\Tests\BrowserTestBase;
 use Drush\TestTraits\DrushTestTrait;
 
 /**
- * Note: Drush must be installed. See
- * https://cgit.drupalcode.org/devel/tree/drupalci.yml?h=8.x-2.x and its docs
+ * Note: Drush must be installed.
+ *
+ * See https://cgit.drupalcode.org/devel/tree/drupalci.yml?h=8.x-2.x and its docs
  * at
- * https://www.drupal.org/drupalorg/docs/drupal-ci/customizing-drupalci-testing-for-projects
+ * https://www.drupal.org/drupalorg/docs/drupal-ci/customizing-drupalci-testing-for-projects.
  */
 
 /**
  * @coversDefaultClass \Drupal\lightning\Generators\SubProfileGenerator
  * @group devel
  */
-class SubProfileGeneratorTest extends BrowserTestBase {
+class SubprofileGeneratorTest extends BrowserTestBase {
 
   use DrushTestTrait;
 
@@ -26,18 +26,13 @@ class SubProfileGeneratorTest extends BrowserTestBase {
    */
   protected $defaultTheme = 'stark';
 
-  //  /**
-  //   * {@inheritdoc}
-  //   */
-  //  protected $profile = 'lightning';
-
   /**
    * Tests drush commands.
    */
   public function testCommands() {
     $this->config('core.extension')->set('module.lightning', 0)->set('profile', 'lightning')->save();
     $this->assertFalse(file_exists('profiles/custom/wizards/wizards.info.yml'));
-    $this->drush('generate', ['lightning-subprofile'], ['answers' => '{"name": "Wizards", "machine_name": "wizards", "description": "This is the description.", "install": "- consumers", "exclusions": "No"}']);
+    $this->drush('generate', ['lightning-subprofile'], ['answers' => '{"name": "Wizards", "machine_name": "wizards", "description": "This is the description.", "exclusions": "No"}']);
     $output = $this->getOutput();
     $this->assertContains('profiles/custom/wizards/wizards.info.yml', $output);
     $this->assertTrue(file_exists('profiles/custom/wizards/wizards.info.yml'));
