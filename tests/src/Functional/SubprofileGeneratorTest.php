4374ec276d3b5c59586571ef0ddd014e4eccd430
kdruckman <kdruckman@gmail.com>
2020-04-02 12:09:58 -0500

    wip

--- tests/src/Functional/SubprofileGeneratorTest.php
+++ tests/src/Functional/SubprofileGeneratorTest.php
@@ -17,7 +17,7 @@ use Drush\TestTraits\DrushTestTrait;
  * @coversDefaultClass \Drupal\lightning\Generators\SubProfileGenerator
  * @group devel
  */
-class SubprofileGeneratorTest extends BrowserTestBase {
+class SubProfileGeneratorTest extends BrowserTestBase {
 
   use DrushTestTrait;
 
@@ -26,18 +26,22 @@ class SubprofileGeneratorTest extends BrowserTestBase {
    */
   protected $defaultTheme = 'stark';
 
-  /**
-   * {@inheritdoc}
-   */
-  protected $profile = 'lightning';
+  //  /**
+  //   * {@inheritdoc}
+  //   */
+  //  protected $profile = 'lightning';
 
   /**
    * Tests drush commands.
    */
   public function testCommands() {
-    $this->drush('generate', ['lightning-subprofile'], ['answers' => '{"name": "Wizards", "machine_name": "wizards", "description": "This is the description.", "install": "consumers", "exclusions": "Yes", "exclude": "2, 3"}']);
+    $this->config('core.extension')->set('module.lightning', 0)->set('profile', 'lightning')->save();
+    $this->assertFalse(file_exists('profiles/custom/wizards/wizards.info.yml'));
+    $this->drush('generate', ['lightning-subprofile'], ['answers' => '{"name": "Wizards", "machine_name": "wizards", "description": "This is the description.", "install": "- consumers", "exclusions": "No"}']);
     $output = $this->getOutput();
     $this->assertContains('profiles/custom/wizards/wizards.info.yml', $output);
+    $this->assertTrue(file_exists('profiles/custom/wizards/wizards.info.yml'));
+    $this->assertContains('name: Wizards', file_get_contents('profiles/custom/wizards/wizards.info.yml'));
   }
 
 }
