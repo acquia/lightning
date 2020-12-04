767047985d493a0bdc436ebbc66f40793405f111
Phéna Proxima <adam@phenaproxima.net>
2020-12-04 14:52:45 -0500

    Implement ORCA 3.

--- tests/travis/install.sh
+++ tests/travis/install.sh
@@ -17,7 +17,7 @@ source ../../../orca/bin/travis/_includes.sh
 # Handle the special case of scanning for deprecations in contrib dependencies.
 # We need to ensure that the components are included as part of the SUT,
 # but none of the other Acquia product modules are.
-if [[ "$ORCA_JOB" == "DEPRECATED_CODE_SCAN_CONTRIB" ]]; then
+if [[ "$ORCA_JOB" == "DEPRECATED_CODE_SCAN_W_CONTRIB" ]]; then
   export ORCA_PACKAGES_CONFIG=../lightning/tests/packages.yml
   orca fixture:init -f --sut="$ORCA_SUT_NAME" --dev --no-site-install
   exit 0
@@ -26,7 +26,7 @@ fi
 # When testing the SUT in isolation using dev package versions, treat the
 # components as part of the SUT, to be installed in an isolated (SUT-only)
 # fixture.
-if [[ "$ORCA_JOB" == "ISOLATED_DEV" ]]; then
+if [[ "$ORCA_JOB" == "ISOLATED_TEST_ON_CURRENT_DEV" ]]; then
   export ORCA_PACKAGES_CONFIG=../lightning/tests/packages.yml
   orca fixture:init -f --sut="$ORCA_SUT_NAME" --dev --profile=lightning
 else
@@ -39,35 +39,3 @@ fi
 
 # Add testing dependencies.
 composer -d"$ORCA_FIXTURE_DIR" require --dev weitzman/drupal-test-traits:dev-master
-
-# Exit early if no DB fixture is specified.
-[[ "$DB_FIXTURE" ]] || exit 0
-
-cd "$ORCA_FIXTURE_DIR/docroot"
-
-DB="$TRAVIS_BUILD_DIR/tests/fixtures/$DB_FIXTURE.php.gz"
-
-php core/scripts/db-tools.php import ${DB}
-
-drush php:script "$TRAVIS_BUILD_DIR/tests/update.php"
-
-drush updatedb --yes
-drush update:lightning --no-interaction --yes
-
-# Do manual updates required for existing site tests to pass, but which
-# do not have automatic scripts.
-drush role:perm:add layout_manager 'configure any layout'
-drush theme:enable claro
-
-orca fixture:enable-extensions
-
-# Reinstall from exported configuration to prove that it's coherent.
-drush config:export --yes
-drush site:install --yes --existing-config
-
-# Disable the History module during testing, to prevent weird deadlock errors
-# that don't actually affect anything.
-drush pm:uninstall --yes history
-
-# Set the fixture state to reset to between tests.
-orca fixture:backup --force
