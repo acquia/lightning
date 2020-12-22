11e7410d26ab2dbdd130cca6c0934f9d6122cc64
Phéna Proxima <adam@phenaproxima.net>
2020-12-22 15:47:26 -0500

    WIP

--- src/Commands/Uninstaller.php
+++ src/Commands/Uninstaller.php
@@ -3,9 +3,47 @@
 namespace Drupal\lightning\Commands;
 
 use Consolidation\AnnotatedCommand\CommandData;
+use Drupal\Core\Extension\ModuleUninstallValidatorException;
+use Drupal\lightning\ExtensionLocationValidator;
+use Drupal\lightning\SubProfileValidator;
 use Drush\Commands\DrushCommands;
 
+/**
+ * Hooks into Drush to assist with uninstalling Lightning.
+ *
+ * @internal
+ *   This class is a completely internal part of Lightning's uninstall system
+ *   and can be changed in any way, or removed outright, at any time without
+ *   warning. External code should not use this class in any way.
+ */
 final class Uninstaller extends DrushCommands {
+
+  /**
+   * Lightning's uninstall validator to check extension locations.
+   *
+   * @var \Drupal\lightning\ExtensionLocationValidator
+   */
+  private $extensionLocationValidator;
+
+  /**
+   * Lightning's uninstall validator to detect sub-profiles.
+   *
+   * @var \Drupal\lightning\SubProfileValidator
+   */
+  private $subProfileValidator;
+
+  /**
+   * Uninstaller constructor.
+   *
+   * @param \Drupal\lightning\ExtensionLocationValidator $extension_location_validator
+   *   Lightning's uninstall validator to check extension locations.
+   * @param \Drupal\lightning\SubProfileValidator $sub_profile_validator
+   *   Lightning's uninstall validator to detect sub-profiles.
+   */
+  public function __construct(ExtensionLocationValidator $extension_location_validator, SubProfileValidator $sub_profile_validator) {
+    $this->extensionLocationValidator = $extension_location_validator;
+    $this->subProfileValidator = $sub_profile_validator;
+  }
 
   /**
    * @hook validate pm:uninstall
@@ -20,6 +58,25 @@ final class Uninstaller extends DrushCommands {
     if (in_array('lightning', $arguments['modules'], TRUE)) {
       if (count($arguments['modules']) > 1) {
         throw new \LogicException('You cannot uninstall Lightning and other extensions at the same time.');
+      }
+
+      $reasons = $this->extensionLocationValidator->validate('lightning');
+      if ($reasons) {
+        $reason = reset($reasons);
+        throw new ModuleUninstallValidatorException($reason);
+      }
+
+      $reasons = $this->subProfileValidator->validate('lightning');
+      if ($reasons) {
+        $this->io()->warning($reasons);
+
+        $decouple = $this->confirm('These profiles can be automatically decoupled from Lightning. Should I do that now?', TRUE);
+        if ($decouple) {
+          $this->subProfileDecoupler->decoupleAll();
+        }
+        else {
+          throw new ModuleUninstallValidatorException('These profiles must be decoupled from Lightning before uninstallation can continue.');
+        }
       }
     }
   }
---
+++ src/SubProfileValidator.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\lightning;
+
+use Drupal\Core\Extension\ModuleUninstallValidatorInterface;
+use Drupal\Core\Extension\ProfileExtensionList;
+
+/**
+ * Validates that no install profiles use Lightning as their parent.
+ *
+ * @internal
+ *   This class is a completely internal part of Lightning's uninstall system
+ *   and can be changed in any way, or removed outright, at any time without
+ *   warning. External code should not use this class in any way.
+ */
+final class SubProfileValidator implements ModuleUninstallValidatorInterface {
+
+  /**
+   * The profile extension list.
+   *
+   * @var \Drupal\Core\Extension\ProfileExtensionList
+   */
+  private $profileList;
+
+  /**
+   * SubProfileValidator constructor.
+   *
+   * @param \Drupal\Core\Extension\ProfileExtensionList $profile_list
+   *   The profile extension list.
+   */
+  public function __construct(ProfileExtensionList $profile_list) {
+    $this->profileList = $profile_list;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($module) {
+    $reasons = [];
+
+    if ($module === 'lightning') {
+      $profiles = $this->listAll();
+      if ($profiles) {
+        $reasons[] = sprintf('The following install profiles use Lightning as a base profile. They must stand alone, or use a different base profile, before Lightning can be uninstalled: %s', implode(', ', $profiles));
+      }
+    }
+    return $reasons;
+  }
+
+  /**
+   * Returns the names of profiles that use Lightning as their parent.
+   *
+   * @return string[]
+   *   The machine names of the profiles (installed or not) that use Lightning
+   *   as their parent.
+   */
+  public function listAll() : array {
+    $filter = function (array $info) : bool {
+      return isset($info['base profile']) && $info['base profile'] === 'lightning';
+    };
+    $profiles = array_filter($this->profileList->getAllAvailableInfo(), $filter);
+    return array_keys($profiles);
+  }
+
+}
