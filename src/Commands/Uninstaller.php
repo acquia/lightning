3d74e1d7a86c721caf55c07a062fc5fc6b7dcc8b
Phéna Proxima <adam@phenaproxima.net>
2020-12-23 14:50:46 -0500

    Move extension location check into uninstaller

--- src/Commands/Uninstaller.php
+++ src/Commands/Uninstaller.php
@@ -2,15 +2,20 @@
 
 namespace Drupal\lightning\Commands;
 
+use Composer\Json\JsonFile;
 use Consolidation\AnnotatedCommand\CommandData;
 use Drupal\Component\Serialization\Yaml;
 use Drupal\Core\Config\FileStorage;
 use Drupal\Core\Config\InstallStorage;
+use Drupal\Core\Extension\Extension;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Extension\ModuleUninstallValidatorException;
 use Drupal\Core\Extension\ProfileExtensionList;
+use Drupal\Core\Extension\ThemeHandlerInterface;
 use Drupal\Core\File\Exception\FileExistsException;
 use Drupal\Core\File\FileSystemInterface;
-use Drupal\lightning\ExtensionLocationValidator;
+use Drupal\profile_switcher\ProfileSwitcher;
+use DrupalFinder\DrupalFinder;
 use Drush\Commands\DrushCommands;
 use Symfony\Component\Filesystem\Exception\IOException;
 
@@ -25,11 +30,18 @@ use Symfony\Component\Filesystem\Exception\IOException;
 final class Uninstaller extends DrushCommands {
 
   /**
-   * Lightning's uninstall validator to check extension locations.
+   * The module handler service.
    *
-   * @var \Drupal\lightning\ExtensionLocationValidator
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
    */
-  private $extensionLocationValidator;
+  private $moduleHandler;
+
+  /**
+   * The theme handler service.
+   *
+   * @var \Drupal\Core\Extension\ThemeHandlerInterface
+   */
+  private $themeHandler;
 
   /**
    * The profile extension list.
@@ -46,49 +58,110 @@ final class Uninstaller extends DrushCommands {
   private $fileSystem;
 
   /**
+   * The profile switcher service.
+   *
+   * @var \Drupal\profile_switcher\ProfileSwitcher
+   */
+  private $profileSwitcher;
+
+  /**
+   * The Drupal root.
+   *
+   * @var string
+   */
+  private $appRoot;
+
+  /**
    * Uninstaller constructor.
    *
-   * @param \Drupal\lightning\ExtensionLocationValidator $extension_location_validator
-   *   Lightning's uninstall validator to check extension locations.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler service.
+   * @param \Drupal\Core\Extension\ThemeHandlerInterface $theme_handler
+   *   The theme handler service.
    * @param \Drupal\Core\Extension\ProfileExtensionList $profile_list
    *   The profile extension list.
    * @param \Drupal\Core\File\FileSystemInterface $file_system
    *   The file system service.
    */
-  public function __construct(ExtensionLocationValidator $extension_location_validator, ProfileExtensionList $profile_list, FileSystemInterface $file_system) {
-    $this->extensionLocationValidator = $extension_location_validator;
+  public function __construct(ModuleHandlerInterface $module_handler, ThemeHandlerInterface $theme_handler, ProfileExtensionList $profile_list, FileSystemInterface $file_system, ProfileSwitcher $profile_switcher, $app_root) {
+    $this->moduleHandler = $module_handler;
+    $this->themeHandler = $theme_handler;
     $this->profileList = $profile_list;
     $this->fileSystem = $file_system;
+    $this->profileSwitcher = $profile_switcher;
+    $this->appRoot = $app_root;
+  }
+
+  private function isActive() : bool {
+    $arguments = $this->input()->getArguments();
+    return in_array('lightning', $arguments['module'], TRUE);
   }
 
   /**
+   * Validates the pm:uninstall command if Lightning is being uninstalled.
+   *
    * @hook validate pm:uninstall
    *
    * @throws \LogicException
    *   Thrown if the user attempts to uninstall any other extension(s) at the
    *   same time as Lightning.
    * @throws \Drupal\Core\Extension\ModuleUninstallValidatorException
-   *   Thrown if one of Lightning's uninstall validators fails and no automatic
-   *   action can be taken to correct the problem.
+   *   Thrown if the Lightning directory contains any installed extensions.
+   * @throws \Drupal\Core\Extension\ModuleUninstallValidatorException
+   *   Thrown if any install profiles exist that are direct children of
+   *   Lightning, and the user decides not to automatically rebuild them.
    */
   public function validate(CommandData $data) : void {
-    $arguments = $data->arguments();
-
-    if (in_array('lightning', $arguments['modules'], TRUE)) {
-      if (count($arguments['modules']) > 1) {
+    if ($this->isActive()) {
+      if (count($data->arguments()) > 1) {
         throw new \LogicException('You cannot uninstall Lightning and other extensions at the same time.');
       }
 
-      $reasons = $this->extensionLocationValidator->validate('lightning');
-      if ($reasons) {
-        $reason = reset($reasons);
-        throw new ModuleUninstallValidatorException($reason);
+      // Ensure that there are no installed modules or themes in the Lightning
+      // profile directory.
+      $extensions = $this->getExtensionsInProfileDirectory();
+      if ($extensions) {
+        $error = sprintf('The following modules and/or themes are located inside the Lightning profile directory. They must be moved elsewhere before Lightning can be uninstalled: %s', implode(', ', $extensions));
+        throw new ModuleUninstallValidatorException($error);
       }
 
       if ($this->decoupleProfiles() == FALSE) {
         throw new ModuleUninstallValidatorException('These profiles must be decoupled from Lightning before uninstallation can continue.');
       }
     }
+  }
+
+  /**
+   * Performs required actions before the Lightning is uninstalled.
+   *
+   * @hook pre-command pm:uninstall
+   */
+  public function preCommand() : void {
+    if ($this->isActive()) {
+      $target = $this->locateProjectFile();
+      $this->io()->write("Rewriting $target...");
+      $this->alterProject($target);
+
+      $profile = 'minimal';
+      $this->io()->write("Switching to $profile profile...");
+      $this->profileSwitcher->switchProfile($profile);
+    }
+  }
+
+  private function getExtensionsInProfileDirectory() : array {
+    $extensions = array_merge(
+      $this->moduleHandler->getModuleList(),
+      $this->themeHandler->listInfo()
+    );
+
+    $profile_path = $extensions['lightning']->getPath();
+    unset($extensions['lightning']);
+
+    $filter = function (Extension $extension) use ($profile_path) : bool {
+      return strpos($extension->getPath(), $profile_path) !== FALSE;
+    };
+    $extensions = array_filter($extensions, $filter);
+    return array_keys($extensions);
   }
 
   /**
@@ -262,7 +335,7 @@ final class Uninstaller extends DrushCommands {
         $this->fileSystem->copy($source, $destination, FileSystemInterface::EXISTS_ERROR);
       }
       catch (FileExistsException $e) {
-        $this->io()->warning($e->getMessage());
+        $this->io()->note($e->getMessage());
       }
     }
   }
@@ -295,6 +368,267 @@ final class Uninstaller extends DrushCommands {
       }
     }
     return $list;
+  }
+
+  private function locateProjectFile() : string {
+    $finder = new DrupalFinder();
+    if ($finder->locateRoot($this->appRoot) == FALSE) {
+      throw new \LogicException("Could not locate the Drupal root.");
+    }
+
+    $target = $finder->getComposerRoot() . DIRECTORY_SEPARATOR . 'composer.json';
+    assert(file_exists($target), "Expected $target to exist, but it does not.");
+
+    return $target;
+  }
+
+  /**
+   * Alters the project-level composer.json to uninstall Lightning.
+   *
+   * @param string $target
+   *   The path of the project-level composer.json.
+   */
+  private function alterProject(string $target) : void {
+    $file = new JsonFile($target);
+    $target = $file->read();
+
+    // Read Lightning's composer.json, since we will need to merge in many
+    // default values from it.
+    $source = new JsonFile(__DIR__ . '/../../composer.json');
+    $source = $source->read();
+    $source += [
+      'extra' => [],
+    ];
+    // Sanity check that we have the correct composer.json.
+    assert($source['name'] === 'acquia/lightning');
+
+    $data = $this->mergeCanadian($target, [
+      'require' => $this->getRequirements($target, $source),
+      'extra' => [
+        'composer-exit-on-patch-failure' => $extra['composer-exit-on-patch-failure'] ?? TRUE,
+        'drupal-scaffold' => [
+          'locations' => [
+            'web-root' => $this->getDrupalRoot($target, $source) . '/',
+          ],
+        ],
+        'enable-patching' => $source['extra']['enable-patching'] ?? TRUE,
+        'installer-paths' => $this->getPaths($target, $source),
+        'installer-types' => $this->getPackageTypes($target),
+        'patchLevel' => $source['extra']['patchLevel'] ?? [],
+        'patches' => $source['extra']['patches'] ?? [],
+        'patches-ignore' => $source['extra']['patches-ignore'] ?? [],
+      ],
+      'repositories' => $this->getRepositories($target),
+    ]);
+
+    // Delete any empty arrays, since they will be encoded as empty arrays and
+    // may therefore break the composer.json schema.
+    $data = array_filter($data, function ($item) {
+      return is_array($item) ? (bool) $item : TRUE;
+    });
+
+    $file->write($data);
+  }
+
+  /**
+   * Returns the combined requirements for the target package.
+   *
+   * @param array $target
+   *   The target package's configuration.
+   * @param array $source
+   *   The source package's configuration.
+   *
+   * @return array
+   *   The combined requirements to add to the target package. The keys will
+   *   be package names and the values will be version constraints.
+   */
+  private function getRequirements(array $target, array $source) : array {
+    $requirements = [];
+    // The target package's existing dependencies should supersede any
+    // dependencies defined by the source package (Lightning).
+    $requirements += ($target['require'] ?? []);
+    $requirements += ($source['require'] ?? []);
+
+    // If the target package is not using the deprecated scaffold plugin, use
+    // the one that ships with Drupal core. On the other hand, if the target
+    // package *is* using the deprecated plugin, they are on their own.
+    if (empty($requirements['drupal-composer/drupal-scaffold'])) {
+      $requirements += [
+        'drupal/core-composer-scaffold' => $requirements['drupal/core'],
+      ];
+    }
+    return $requirements;
+  }
+
+  /**
+   * Returns the package types to expose to the Composer installers extender.
+   *
+   * @param array $target
+   *   The target package's configuration.
+   *
+   * @return string[]
+   *   The package types to expose to the Composer installers extender plugin
+   *   (oomphinc/composer-installers-extender), if available.
+   */
+  private function getPackageTypes(array $target) : array {
+    $target += [
+      'extra' => [],
+    ];
+    $installer_types = $target['extra']['installer-types'] ?? [];
+
+    // Ensure that npm-asset and bower-asset are known package types.
+    array_push($installer_types, 'npm-asset', 'bower-asset');
+    return array_unique($installer_types);
+  }
+
+  /**
+   * Returns the combined installer paths for the target package.
+   *
+   * @param array $target
+   *   The target package's configuration.
+   * @param array $source
+   *   The source package's configuration.
+   *
+   * @return array[]
+   *   An array of paths to be used by the composer/installers plugin.
+   */
+  private function getPaths(array $target, array $source) : array {
+    $root_dir = $this->getDrupalRoot($target, $source);
+    // If we don't know where Drupal core is installed, we cannot possibly
+    // determine where modules, themes, etc. should go.
+    if (empty($root_dir)) {
+      throw new \LogicException("Cannot determine the Drupal root.");
+    }
+
+    $path_map = $this->getPathMap($target, $source);
+    $path_map += [
+      'type:drupal-module' => $root_dir . '/modules/contrib/{$name}',
+      'type:drupal-custom-module' => $root_dir . '/modules/custom/{$name}',
+      'type:drupal-profile' => $root_dir . '/profiles/contrib/{$name}',
+      'type:drupal-theme' => $root_dir . '/themes/contrib/{$name}',
+      'type:drupal-custom-theme' => $root_dir . '/themes/custom/{$name}',
+      'type:drupal-library' => $root_dir . '/libraries/{$name}',
+      'type:npm-asset' => $root_dir . '/libraries/{$name}',
+      'type:bower-asset' => $root_dir . '/libraries/{$name}',
+    ];
+
+    $paths = [];
+    foreach ($path_map as $package => $location) {
+      $paths[$location][] = $package;
+    }
+    return $paths;
+  }
+
+  /**
+   * Returns the combined repositories for the target package.
+   *
+   * @param array $target
+   *   The target package's configuration.
+   *
+   * @return array[]
+   *   An array of Composer repository definitions to add to the target package.
+   */
+  private function getRepositories(array $target) : array {
+    $repositories = [];
+
+    $source_repositories = [
+      'https://packages.drupal.org/8',
+      'https://asset-packagist.org',
+    ];
+
+    $target_repositories = [];
+    foreach (($target['repositories'] ?? []) as $repository) {
+      if ($repository['type'] === 'composer') {
+        $target_repositories[] = $repository['url'];
+      }
+    }
+
+    // Ensure that the two repositories listed in $source_repositories are
+    // added to the target package's repositories.
+    $repositories_to_add = array_diff($source_repositories, $target_repositories);
+
+    foreach ($repositories_to_add as $url) {
+      $repositories[] = [
+        'type' => 'composer',
+        'url' => $url,
+      ];
+    }
+    return $repositories;
+  }
+
+  /**
+   * Returns a map of locations where packages will be installed.
+   *
+   * @param array $target
+   *   The target package's configuration.
+   * @param array $source
+   *   The source package's configuration.
+   *
+   * @return string[]
+   *   A map where the keys are the package, or package type, to install (e.g.,
+   *   'drupal/dropzonejs' or 'type:drupal-theme') and the values are the
+   *   location where that package or package type will be installed, relative
+   *   to the target package.
+   */
+  private function getPathMap(array $target, array $source) : array {
+    // Try to get the installer-paths configuration from the target package,
+    // falling back to the source package (Lightning) in the unlikely event
+    // that the target package has not configured this.
+    $extra = isset($target['extra']['installer-paths'])
+      ? $target['extra']
+      : $source['extra'];
+
+    $path_map = [];
+    foreach ($extra['installer-paths'] as $location => $packages) {
+      foreach ($packages as $package) {
+        $path_map[$package] = $location;
+      }
+    }
+    return $path_map;
+  }
+
+  /**
+   * Returns the path to the Drupal root, relative to the target package.
+   *
+   * @param array $target
+   *   The target package's configuration.
+   * @param array $source
+   *   The source package (i.e., Lightning)'s configuration.
+   *
+   * @return string|null
+   *   The path to the Drupal root, relative to the target package, e.g.,
+   *   'docroot', or NULL if it cannot be determined.
+   */
+  private function getDrupalRoot(array $target, array $source) : ?string {
+    $path_map = $this->getPathMap($target, $source);
+
+    // We expect that the path map has an install location for Drupal core. If
+    // it doesn't, that's a pretty major error condition; in such a case, it's
+    // not clear how their code base could even be working. Maybe it's a bizarre
+    // set-up (symlink jungle?) that we don't support.
+    $core_location = $path_map['drupal/core'] ?? $path_map['type:drupal-core'];
+    return $core_location ? dirname($core_location) : NULL;
+  }
+
+  /**
+   * Recursively merges two associative arrays, preserving existing items.
+   *
+   * @param array $a
+   *   The array which $b will be merged into.
+   * @param array $b
+   *   The array to merge into $a.
+   *
+   * @return array
+   *   The merged array.
+   */
+  private function mergeCanadian(array $a, array $b) : array {
+    $a += $b;
+    foreach ($a as $k => $v) {
+      if (is_array($v) && isset($b[$k]) && is_array($b[$k])) {
+        $a[$k] = $this->mergeCanadian($a[$k], $b[$k]);
+      }
+    }
+    return $a;
   }
 
 }
