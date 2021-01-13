4cc8b122d5325f3dcf36c14657e330500eb5d2e6
Phéna Proxima <adam@phenaproxima.net>
2021-01-13 09:41:43 -0500

    Simplify tarball build scripts to require Lightning directly, rather than the individual components.

--- src/Composer/ConfigureLegacyProject.php
+++ src/Composer/ConfigureLegacyProject.php
@@ -22,21 +22,6 @@ final class ConfigureLegacyProject {
     $target = new JsonFile($arguments[0] . '/composer.json');
     $project = $target->read();
 
-    $required = $event->getComposer()->getPackage()->getRequires();
-    $components = [
-      'api',
-      'core',
-      'media',
-      'layout',
-      'workflow',
-    ];
-    foreach ($components as $component) {
-      $project['require']["drupal/lightning_$component"] = $required["drupal/lightning_$component"]->getPrettyConstraint();
-    }
-    $project['repositories'][] = [
-      'type' => 'composer',
-      'url' => 'https://asset-packagist.org',
-    ];
     $project['extra']['installer-paths']['libraries/{$name}'] = [
       'type:drupal-library',
       'type:bower-asset',
@@ -44,6 +29,7 @@ final class ConfigureLegacyProject {
     ];
     $project['extra']['installer-types'] = ['bower-asset', 'npm-asset'];
     $project['extra']['patchLevel']['drupal/core'] = '-p2';
+    $project['extra']['enable-patching'] = TRUE;
 
     $target->write($project);
   }
