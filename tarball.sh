21c2229720b864579819efca2462e708ec1b4fd4
Phéna Proxima <adam@phenaproxima.net>
2021-01-13 09:41:43 -0500

    Simplify tarball build scripts to require Lightning directly, rather than the individual components.

--- tarball.sh
+++ tarball.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
 
 ARCHIVE=lightning-8.x-$1
-PROFILE_DIR=profiles/contrib/lightning
-YAML_CLI=`command -v yaml-cli`
-
-# Ensure yaml-cli is installed, since we need it to set version numbers
-# in the info files.
-if [[ ! $YAML_CLI ]]; then
-  echo "Cannot set version in info files because yaml-cli is not in your PATH."
-  exit 1
-fi
 
 composer create-project --no-install drupal/legacy-project:^8.9.10 $ARCHIVE
 composer dump-autoload
 composer configure-tarball $ARCHIVE
 
-# Update version number in info files.
-find . -name "*.info.yml" -exec $YAML_CLI update:value {} version 8.x-$1 \;
-
-# Create an archive of the profile to be added to the tarball.
-composer archive --file $ARCHIVE
-
-# Remove modifications to info files.
-git reset --hard
-
 cd $ARCHIVE
-composer config extra.enable-patching true
 composer config minimum-stability dev
 composer config prefer-stable true
+composer config repositories.assets composer https://asset-packagist.org
 composer remove --no-update composer/installers
-composer require --no-update "ext-dom:*" cweagans/composer-patches oomphinc/composer-installers-extender
+composer require --no-update "ext-dom:*" "acquia/lightning:~3.4.0" cweagans/composer-patches
 composer update
 
-# Create the profile destination directory.
-mkdir -p $PROFILE_DIR
-
-# Extract the profile archive into it.
-tar -x -f ../$ARCHIVE.tar --directory $PROFILE_DIR
-cd ..
-
 # Wrap it all up in a nice compressed tarball.
+cd ..
 tar --exclude='.DS_Store' --exclude='._*' -c -z -f $ARCHIVE.tar.gz $ARCHIVE
 
 # Clean up.
