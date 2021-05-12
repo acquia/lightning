5ab249a981ca755df2186f9c236c009ed2e15e67
phenaproxima <adam@phenaproxima.net>
2021-05-12 11:23:59 -0400

    Do it in Travis config.

--- tests/travis/install.sh
+++ tests/travis/install.sh
@@ -30,7 +30,6 @@ if [[ "$ORCA_JOB" == "ISOLATED_TEST_ON_CURRENT_DEV" ]]; then
   export ORCA_PACKAGES_CONFIG=../lightning/tests/packages.yml
   orca fixture:init -f --sut="$ORCA_SUT_NAME" --dev --profile=lightning
 else
-  composer require "drupal/core:^9.1.7" --no-update
   # Run ORCA's standard installation script.
   ../../../orca/bin/travis/install.sh
 fi
